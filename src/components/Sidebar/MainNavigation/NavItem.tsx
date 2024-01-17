b89be700ac61fb3c29eeb7e4d9b215a92be31804
vitorlinsbinski <vitor.linsbinski@hotmail.com>
2024-01-17 10:29:29 -0400

    first commit

---
+++ src/components/Sidebar/MainNavigation/NavItem.tsx
@@ -0,0 +1,22 @@
+import { ChevronDown } from "lucide-react";
+import { ElementType } from "react";
+
+interface NavItemProps {
+  title: string;
+  icon: ElementType;
+}
+
+export function NavItem({ icon: Icon, title }: NavItemProps) {
+  return (
+    <a
+      href=""
+      className="group flex items-center gap-3 rounded px-3 py-2 transition-colors duration-200 hover:bg-violet-50"
+    >
+      <Icon className="h-5 w-5 text-zinc-500" />
+      <span className="font-medium text-zinc-700 transition-colors duration-200 group-hover:text-violet-500">
+        {title}
+      </span>
+      <ChevronDown className="ml-auto h-5 w-5 text-zinc-400 transition-colors duration-200 group-hover:text-violet-300" />
+    </a>
+  );
+}
